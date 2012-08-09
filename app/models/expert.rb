11cfe4bdb4726ad2c022eaa665c8c1410b0e42d0
ushi <ushi@porkbox.net>
2012-08-09 09:34:30 +0200

    Added constant values for degree attribute
    * The experts attribute degree can only have on of the following values:
      nil, :bachelor, :master or :phd

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -36,7 +36,10 @@ class Expert < ActiveRecord::Base
   belongs_to :user
 
   # Set of vailable genders.
-  GENDERS = Set.new([:female, :male])
+  GENDERS = [:female, :male].to_set
+
+  # Set of available degrees.
+  DEGREES = [:bachelor, :master, :phd].to_set
 
   # Returns a valid gender symbol using the GENDERS list.
   #
@@ -47,6 +50,12 @@ class Expert < ActiveRecord::Base
   def self.gender(gender)
     g = gender.try(:to_sym)
     GENDERS.include?(g) ? g : GENDERS.first
+  end
+
+  # Returns a valid degree symbol or nil for invalid values.
+  def self.degree(degree)
+    d = degree.try(:to_sym)
+    DEGREES.include?(d) ? d : nil
   end
 
   # Initializes the contact on access, if not already initalized.
@@ -67,7 +76,17 @@ class Expert < ActiveRecord::Base
   # Sets the experts gender. If the given gender is invalid, a default value
   # is assigned.
   def gender=(gender)
-    super(Expert.gender(gender).to_s)
+    super(Expert.gender(gender))
+  end
+
+  # Returns the experts degree
+  def degree
+    Expert.degree(super)
+  end
+
+  # Sets the experts degree. If the given value is invalid, nil is assigned.
+  def degree=(degree)
+    super(Expert.degree(degree))
   end
 
   # Sets the experts languages. So we can do things like:
