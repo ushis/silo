f4914460567c1f60323fbd7b6c4715f375e80e1d
ushi <ushi@porkbox.net>
2012-08-23 23:15:46 +0200

    Added documentation.

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,12 +1,18 @@
+# The HelpController serves help views. It is intended that they were loaded
+# via AJAX, so they were not rendered within a layout.
 #
+# To add a new help section create the view
+# _app/views/help/my_section.html.erb_. You can vistit it at
+# _/help/my_section_.
 class HelpController < ApplicationController
   skip_before_filter :authorize
 
   layout false
 
+  # Serves a help section.
   def show
     render params[:section]
-  rescue
+  rescue ActionView::MissingTemplate
     raise ActionController::RoutingError, 'Help not found.'
   end
 end
