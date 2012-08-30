f040ac7e8d33caebe0384b44d2f28bf3d2d27e21
ushi <ushi@porkbox.net>
2012-08-31 01:31:00 +0200

    Fixed ApplicationController#locale_from_header
    * Fixed the case where the LANGUAGE-ACCEPT header is nil.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,6 +80,6 @@ class ApplicationController < ActionController::Base
 
   # Extracts the preferred locale from the ACCEPT-LANGUAGE header.
   def locale_from_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    request.env['HTTP_ACCEPT_LANGUAGE'].to_s.scan(/^[a-z]{2}/).first
   end
 end
