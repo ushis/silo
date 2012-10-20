6594f389868393edeea61095063af61ddb50c6d8
ushi <ushi@porkbox.net>
2012-10-20 04:46:58 +0200

    Improved UserSweeper.
    Only expire user caches if the name or the prename of the user has
    changed.

--- app/sweepers/user_sweeper.rb
+++ app/sweepers/user_sweeper.rb
@@ -2,13 +2,29 @@
 class UserSweeper < ActionController::Caching::Sweeper
   observe User
 
-  # Expires all users/select caches.
+  # Expires caches when a new user was created.
   def after_create(user)
+    expire_caches
+  end
+
+  # Expires caches when the name or the prename of a user was changed.
+  def after_update(user)
+    if user.name_changed? || user.prename_changed?
+      expire_caches
+    end
+  end
+
+  # Expires caches when a user was destroyed.
+  def after_destroy(user)
+    expire_caches
+  end
+
+  private
+
+  # Expires all users/select caches.
+  def expire_caches
     I18n.available_locales.each do |locale|
       expire_fragment([locale, :users, :select].join('/'))
     end
   end
-
-  alias after_update after_create
-  alias after_destroy after_create
 end
