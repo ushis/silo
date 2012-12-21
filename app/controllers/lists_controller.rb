87f5bffcbc7ae61c35ecbdf1502ca8b3476c51f9
ushi <ushi@porkbox.net>
2012-12-21 19:24:06 +0100

    Refactored ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,8 @@
 # The ListsController provides the actions to handle some list specific
 # requests. See Ajax::LitsController to find more.
 class ListsController < ApplicationController
-  before_filter :check_password, only: [:destroy]
+  before_filter :check_password, only: :destroy
+  before_filter :find_list,      only: [:update, :concat, :copy, :destroy]
 
   skip_before_filter :authorize
 
@@ -28,67 +29,62 @@ class ListsController < ApplicationController
 
   # Updates a list.
   def update
-    list = List.find_for_user(params[:id], current_user)
-
-    if list.update_attributes(params[:list])
+    if @list.update_attributes(params[:list])
       flash[:notice] = t('messages.list.success.save')
     else
       flash[:alert] = t('messages.list.errors.save')
     end
 
-    redirect_to list_experts_url(list)
+    redirect_to list_experts_url(@list)
   end
 
   # Concats a list with another.
   def concat
-    list = List.find_for_user(params[:id], current_user)
     other = List.find_for_user(params[:other], current_user)
-    list.concat(other)
+    @list.concat(other)
     flash[:notice] = t('messages.list.success.concat', title: other.title)
-    redirect_to list_experts_url(list)
-  rescue ActiveRecord::RecordNotFound, UnauthorizedError
-    raise unless list
-    flash[:alert] = t('messages.list.errors.find')
-    redirect_to list_experts_url(list)
+    redirect_to list_experts_url(@list)
   end
 
   # Copies a list.
   def copy
-    original = List.find_for_user(params[:id], current_user)
-    copy = original.copy
+    copy = @list.copy
     copy.comment = Comment.new(params[:list].try(:delete, :comment_attributes))
     copy.attributes = params[:list]
     copy.user = current_user
     copy.private = true
 
     if copy.save
-      flash[:notice] = t('messages.list.success.copy', title: original.title)
+      flash[:notice] = t('messages.list.success.copy', title: @list.title)
       redirect_to list_experts_url(copy)
     else
       flash[:alert] = t('messages.list.errors.copy')
-      redirect_to list_experts_url(original)
+      redirect_to list_experts_url(@list)
     end
   end
 
   # Destroys a list.
   def destroy
-    list = List.find_for_user(params[:id], current_user)
-
-    if list.destroy
-      flash[:notice] = t('messages.list.success.delete', title: list.title)
+    if @list.destroy
+      flash[:notice] = t('messages.list.success.delete', title: @list.title)
       redirect_to lists_url
     else
       flash[:alert] = t('messages.list.errors.delete')
-      redirect_to list_experts_url(list)
+      redirect_to list_experts_url(@list)
     end
   end
 
   private
 
-  # Sets a flash and redirects to the lists index.
+  # Finds the list.
+  def find_list
+    @list = List.find_for_user(params[:id], current_user)
+  end
+
+  # Sets a flash and redirects the user.
   def not_found
     flash[:alert] = t('messages.list.errors.find')
-    redirect_to lists_url
+    redirect_to(@list ? list_experts_url(@list) : lists_url)
   end
 
   # Redirects to the lists index.
